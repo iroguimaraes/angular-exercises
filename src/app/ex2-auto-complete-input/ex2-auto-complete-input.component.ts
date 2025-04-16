805932a19d20eb0fca76e9fe59f7d928130214c7
iroguimaraes <betaionly@gmail.com>
2025-04-16 02:07:48 -0300

    finished ex2 and fixed name of buttons for the exercises, added notes at the bottom of exercises

--- src/app/ex2-auto-complete-input/ex2-auto-complete-input.component.ts
+++ src/app/ex2-auto-complete-input/ex2-auto-complete-input.component.ts
@@ -15,8 +15,32 @@ export class Ex2AutoCompleteInputComponent {
   mockList = [
     { id: 1, name: 'Maça', description: '' },
     { id: 2, name: 'Pera', description: '' },
-    { id: 3, name: 'Goiaba', description: '' }
+    { id: 3, name: 'Goiaba', description: '' },
+    { id: 4, name: 'Banana', description: '' },
+    { id: 5, name: 'Uva', description: '' },
+    { id: 6, name: 'Melancia', description: '' },
+    { id: 7, name: 'Morango', description: '' },
+    { id: 8, name: 'Abacaxi', description: '' },
+    { id: 9, name: 'Kiwi', description: '' },
+    { id: 10, name: 'Laranja', description: '' },
+    { id: 11, name: 'Limão', description: '' },
+    { id: 12, name: 'Mamão', description: '' },
+    { id: 13, name: 'Maracujá', description: '' },
+    { id: 14, name: 'Pêssego', description: '' },
+    { id: 15, name: 'Amora', description: '' },
+    { id: 16, name: 'Framboesa', description: '' },
+    { id: 17, name: 'Manga', description: '' },
+    { id: 18, name: 'Cereja', description: '' },
+    { id: 19, name: 'Abacate', description: '' },
+    { id: 20, name: 'Coco', description: '' }
   ];
+
+  filterList!: [
+    {
+      id: number,
+      name: string,
+      description: string,
+    }];
 
   constructor() {
 
@@ -24,5 +48,15 @@ export class Ex2AutoCompleteInputComponent {
 
   onInputChange() {
     this.search = this.test.length > 0; //change to true if meets condition
+    this.checkName();
+  }
+
+  checkName() {
+    this.filterList = [{ id: 0, name: '', description: '' }]
+    for (let i = 0; i < this.mockList.length; i++) {
+      if (this.mockList[i].name.match(this.test)) {
+        this.filterList.push(this.mockList[i])
+      }
+    }
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,8 @@ import { Ex2AutoCompleteInputComponent } from '../ex2-auto-complete-input/ex2-au
 export class HomeComponent {
 
   buttons = [
-    { id: 1, text: Ex1UserProfileFormComponent.name, path: '/ex1', color: 'primary' },
-    { id: 2, text: Ex2AutoCompleteInputComponent.name, path: '/ex2', color: 'primary' },
+    { id: 1, text: 'ex1 - Teste de Databinding', path: '/ex1', color: 'primary' },
+    { id: 2, text: 'ex2 - Auto Complete input', path: '/ex2', color: 'primary' },
   ];
 
   constructor(private router: Router) {
